721848ef351d04f1dfbdda931b96f76299134ee7
Daniel Harrington <149312468+danielharrington06@users.noreply.github.com>
2026-09-24 15:19:12 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alpha = "abcdefghijklmnopqrstuvwxyz"
+        numeric = "0123456789"
+        alphanumeric = alpha + alpha.upper() + numeric
+
+        forward = ""
+        for c in s:
+            if c in alphanumeric:
+                forward += c.lower()
+        
+        reverse = forward[::-1]
+        print(reverse, forward)
+
+        return reverse == forward
